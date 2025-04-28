a4bb51bcef8606619243af1741a40bab16ba7ac0
hdang04 <166868747+hdang04@users.noreply.github.com>
2025-04-27 23:25:04 -0700

    Update supabase.ts

--- DineAuto/utils/supabase.ts
+++ DineAuto/utils/supabase.ts
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { AppState, Platform } from 'react-native';
 
-const supabaseUrl = 'https://zkojouyyalewawzmirvn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inprb2pvdXl5YWxld2F3em1pcnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NjI2MTQsImV4cCI6MjA1OTEzODYxNH0.TZNxXV0T4ibWcmuXoqT6q6toUpTGaKGJ5xhuEloIP8Y';
+const supabaseUrl = ;
+const supabaseAnonKey = ;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
